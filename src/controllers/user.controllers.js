2382d83ce21558e85c180fc6731040dade22ff7c
ShubhamKashyap1001 <shubh9142996613@gmail.com>
2025-07-30 01:28:44 +0530

    update user.controllers.js

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,7 +1,7 @@
 import {User} from '../models/user.model.js';
 import { ApiError } from '../utils/ApiError.js';
 import { ApiResponse } from '../utils/ApiResponse.js';
-import { asyncHandler } from '../utils/AsyncHandler.js';
+import { asyncHandler } from '../utils/asyncHandler.js';
 import bcrypt from 'bcryptjs';
 import jwt from "jsonwebtoken"
 
